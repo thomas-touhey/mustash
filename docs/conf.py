thomas-touhey/lscl

feat: initial commit

docs/conf.py
@@ -1,0 +1,131 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+import sys
+
+from toml import load as load_toml
+
+
+# Add the module path.
+sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent / "_ext"))
+pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
+
+with open(pyproject_path) as pyproject_file:
+    version = load_toml(pyproject_file)["tool"]["poetry"]["version"]
+
+project = "lscl"
+copyright = "2024, Thomas Touhey"
+author = "Thomas Touhey"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinxcontrib.autodoc_pydantic",
+    "sphinxcontrib.mermaid",
+    "lscl_autodoc",
+]
+
+templates_path: list[str] = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_title = f"lscl {version}"
+html_favicon = "_static/favicon.png"
+html_logo = "_static/logo.png"
+html_use_index = False
+html_copy_source = False
+html_show_sourcelink = False
+html_domain_indices = False
+html_css_files = ["custom.css"]
+
+intersphinx_mapping: dict[str, tuple[str, None]] = {
+    "python": ("https://docs.python.org/3", None),
+    "pydantic": ("https://docs.pydantic.dev/latest", None),
+}
+
+todo_include_todos = True
+
+mermaid_output_format = "raw"
+mermaid_init_js = """
+function isDarkMode() {
+    const color = (
+        getComputedStyle(document.body)
+        .getPropertyValue("--color-code-foreground")
+    );
+
+    if (color == "#d0d0d0")
+        return true;
+
+    return false;
+}
+
+function initializeMermaid(isStart) {
+    mermaid.initialize({
+        startOnLoad: isStart,
+        theme: isDarkMode() ? "dark" : "base",
+        darkMode: isDarkMode(),
+        securityLevel: "antiscript"
+    });
+}
+
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (
+            mutation.type != "attributes"
+            || mutation.attributeName != "data-theme"
+        )
+            return
+
+        const nodes = document.querySelectorAll(".mermaid");
+        nodes.forEach(node => {
+            /* Restore the original code before reprocessing. */
+            node.innerHTML = node.getAttribute("data-original-code");
+
+            /* Remove the attribute saying data is processed; it is not! */
+            if (node.hasAttribute("data-processed"))
+                node.removeAttribute("data-processed");
+        });
+
+        initializeMermaid(false);
+        mermaid.run({nodes: nodes, querySelector: ".mermaid"});
+    });
+});
+
+(function (window) {
+    /* Store original code for diagrams into an attribute directly, since
+       Mermaid actually completely replaces the content and removes the
+       original code. */
+    document.querySelectorAll(".mermaid").forEach(node => {
+        node.setAttribute("data-original-code", node.innerHTML);
+    })
+
+    initializeMermaid(true);
+    observer.observe(document.body, {attributes: true});
+})(window);
+"""
+
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "inherited-members": "BaseModel",
+    "exclude-members": "model_config, model_fields, model_computed_fields, "
+    + "model_post_init",
+}
+autodoc_member_order = "bysource"
+
+autodoc_pydantic_field_show_constraints = False
+autodoc_pydantic_field_list_validators = False
+autodoc_pydantic_model_show_json = False

tests/__init__.py
@@ -1,0 +1,31 @@
+#!/usr/bin/env python
+# *****************************************************************************
+# Copyright (C) 2024 Thomas Touhey <thomas@touhey.fr>
+#
+# This software is governed by the CeCILL-C license under French law and
+# abiding by the rules of distribution of free software. You can use, modify
+# and/or redistribute the software under the terms of the CeCILL-C license
+# as circulated by CEA, CNRS and INRIA at the following
+# URL: https://cecill.info
+#
+# As a counterpart to the access to the source code and rights to copy, modify
+# and redistribute granted by the license, users are provided only with a
+# limited warranty and the software's author, the holder of the economic
+# rights, and the successive licensors have only limited liability.
+#
+# In this respect, the user's attention is drawn to the risks associated with
+# loading, using, modifying and/or developing or reproducing the software by
+# the user in light of its specific status of free software, that may mean
+# that it is complicated to manipulate, and that also therefore means that it
+# is reserved for developers and experienced professionals having in-depth
+# computer knowledge. Users are therefore encouraged to load and test the
+# software's suitability as regards their requirements in conditions enabling
+# the security of their systems and/or data to be ensured and, more generally,
+# to use and operate it in the same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL-C license and that you accept its terms.
+# *****************************************************************************
+"""Unit tests for lscl."""
+
+from __future__ import annotations

thomas-touhey/mustash

docs/conf.py
@@ -1,0 +1,141 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+import sys
+
+from toml import load as load_toml
+
+# Due to a weird bug with Sphinx and pydantic, we want to import the module
+# here instead of later, so that it already imported when generating the
+# autodocs.
+import mustash.es  # noqa
+import mustash.processors  # noqa
+
+
+# Add the module path.
+sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.append(str(Path(__file__).parent / "_ext"))
+pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
+
+with open(pyproject_path) as pyproject_file:
+    version = load_toml(pyproject_file)["tool"]["poetry"]["version"]
+
+project = "Mustash"
+copyright = "2024, Thomas Touhey"
+author = "Thomas Touhey"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinxcontrib.autodoc_pydantic",
+    "sphinxcontrib.mermaid",
+    "mustash_autodoc",
+]
+
+templates_path: list[str] = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_title = f"Mustash {version}"
+html_use_index = False
+html_copy_source = False
+html_show_sourcelink = False
+html_domain_indices = False
+html_css_files = ["custom.css"]
+html_favicon = "_static/favicon.png"
+html_logo = "_static/logo.svg"
+
+intersphinx_mapping: dict[str, tuple[str, None]] = {
+    "python": ("https://docs.python.org/3", None),
+    "pydantic": ("https://docs.pydantic.dev/latest", None),
+}
+
+todo_include_todos = True
+
+mermaid_output_format = "raw"
+mermaid_init_js = """
+function isDarkMode() {
+    const color = (
+        getComputedStyle(document.body)
+        .getPropertyValue("--color-code-foreground")
+    );
+
+    if (color == "#d0d0d0")
+        return true;
+
+    return false;
+}
+
+function initializeMermaid(isStart) {
+    mermaid.initialize({
+        startOnLoad: isStart,
+        theme: isDarkMode() ? "dark" : "base",
+        darkMode: isDarkMode(),
+        securityLevel: "antiscript"
+    });
+}
+
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (
+            mutation.type != "attributes"
+            || mutation.attributeName != "data-theme"
+        )
+            return
+
+        const nodes = document.querySelectorAll(".mermaid");
+        nodes.forEach(node => {
+            /* Restore the original code before reprocessing. */
+            node.innerHTML = node.getAttribute("data-original-code");
+
+            /* Remove the attribute saying data is processed; it is not! */
+            if (node.hasAttribute("data-processed"))
+                node.removeAttribute("data-processed");
+        });
+
+        initializeMermaid(false);
+        mermaid.run({nodes: nodes, querySelector: ".mermaid"});
+    });
+});
+
+(function (window) {
+    /* Store original code for diagrams into an attribute directly, since
+       Mermaid actually completely replaces the content and removes the
+       original code. */
+    document.querySelectorAll(".mermaid").forEach(node => {
+        node.setAttribute("data-original-code", node.innerHTML);
+    })
+
+    initializeMermaid(true);
+    observer.observe(document.body, {attributes: true});
+})(window);
+"""
+
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "inherited-members": "BaseModel",
+    "exclude-members": "model_config, model_fields, model_computed_fields, "
+    + "model_post_init",
+}
+autodoc_type_aliases = {
+    "FieldPathType": "FieldPath",
+    "FieldType": "Element",
+    "_ProcessorType": "Processor",
+}
+autodoc_member_order = "bysource"
+
+autodoc_pydantic_field_list_validators = False
+autodoc_pydantic_model_show_json = False

tests/__init__.py
@@ -1,0 +1,31 @@
+#!/usr/bin/env python
+# *****************************************************************************
+# Copyright (C) 2024 Thomas Touhey <thomas@touhey.fr>
+#
+# This software is governed by the CeCILL-C license under French law and
+# abiding by the rules of distribution of free software. You can use, modify
+# and/or redistribute the software under the terms of the CeCILL-C license
+# as circulated by CEA, CNRS and INRIA at the following
+# URL: https://cecill.info
+#
+# As a counterpart to the access to the source code and rights to copy, modify
+# and redistribute granted by the license, users are provided only with a
+# limited warranty and the software's author, the holder of the economic
+# rights, and the successive licensors have only limited liability.
+#
+# In this respect, the user's attention is drawn to the risks associated with
+# loading, using, modifying and/or developing or reproducing the software by
+# the user in light of its specific status of free software, that may mean
+# that it is complicated to manipulate, and that also therefore means that it
+# is reserved for developers and experienced professionals having in-depth
+# computer knowledge. Users are therefore encouraged to load and test the
+# software's suitability as regards their requirements in conditions enabling
+# the security of their systems and/or data to be ensured and, more generally,
+# to use and operate it in the same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL-C license and that you accept its terms.
+# *****************************************************************************
+"""Unit tests for Mustash."""
+
+from __future__ import annotations